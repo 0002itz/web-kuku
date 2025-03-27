80101846d0b7eefcce6483fa160a638d7679b22e
0002itz <workalias.skedaddle347@passinbox.com>
2025-03-27 12:41:34 -0600

    add css(hiden img from slider)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+		},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addUtilities }) {
+			const newUtilities = {
+				".slider img:not(img: first - child)": {
+					display: "none;"
+				},
+				".expouse_first_img": {
+				},
+			};
+			addUtilities(newUtilities)
+		},
+	],
 }
