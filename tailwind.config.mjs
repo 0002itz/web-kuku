48b481744d620c9c3a3c982a6913362d24dd7e5f
0002itz <workalias.skedaddle347@passinbox.com>
2025-03-31 23:05:40 -0600

    prv slider

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}'],
 	theme: {
 		extend: {
+			slider: {
+				img: "not(img:first-child){display: 'none;'}"
+			},
 		},
 	},
 	plugins: [
 		function ({ addUtilities }) {
 			const newUtilities = {
-				".slider": {
-					img: "not(img:first-child) {display: 'none;'}"
+				slider: {
+					img: "not(img:first-child){display: 'none;'}"
 				},
 			};
 			addUtilities(newUtilities)
